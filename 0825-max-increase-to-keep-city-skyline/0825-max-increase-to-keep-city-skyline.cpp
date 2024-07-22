10f266a7b671d07f3e792d79a516fa030f28332a
Shivansh Gaur <shivanshgaur.bt21ele@pec.edu.in>
2024-07-22 09:09:12 +0530

    Time: 9 ms (30.06%), Space: 12.9 MB (18.41%) - LeetHub

--- 0825-max-increase-to-keep-city-skyline/0825-max-increase-to-keep-city-skyline.cpp
+++ 0825-max-increase-to-keep-city-skyline/0825-max-increase-to-keep-city-skyline.cpp
@@ -4,30 +4,20 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         int cnt = 0;
-        vector<int> rowMax;
-        vector<int> colMax;
-        for(int i =0 ; i < n; i++){
-            int maxi = INT_MIN;
-            for(int j = 0 ; j < m ;j++){
-                maxi = max(maxi, grid[i][j]);
+        vector<int> rowMax(n, 0);
+        vector<int> colMax(m, 0);
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < m; ++j) {
+                rowMax[i] = max(rowMax[i], grid[i][j]);
+                colMax[j] = max(colMax[j], grid[i][j]);
             }
-            rowMax.push_back(maxi);
+        }
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < m; ++j) {
+                cnt += min(rowMax[i], colMax[j]) - grid[i][j];
+            }
         }
 
-        for(int i = 0 ; i < m ; i++){
-            int maxi = INT_MIN;
-            for(int j = 0; j < n; j++){
-                maxi = max(maxi , grid[j][i]);
-            }
-            colMax.push_back(maxi);
-        }
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0 ; j < m ; j++){
-                cout << rowMax[i] << " " << colMax[i] << endl;
-                cnt += abs(grid[i][j] - min(rowMax[i], colMax[j]));
-            }
-        }
         return cnt;
     }
 };
