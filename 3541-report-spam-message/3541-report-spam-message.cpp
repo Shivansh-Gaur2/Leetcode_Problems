ff95ddc95f8ab27684f0414b90ed898fc23d05f8
Shivansh Gaur <shivanshgaur.bt21ele@pec.edu.in>
2024-09-23 21:08:58 +0530

    Time: 512 ms (6.67%), Space: 288.7 MB (6.67%) - LeetHub

---
+++ 3541-report-spam-message/3541-report-spam-message.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
+        unordered_set<string> word(bannedWords.begin(), bannedWords.end());
+
+        int cnt = 0;
+        
+        for(auto it : message){
+            if(word.count(it)){
+                cnt++;
+            }
+        }
+        if(cnt >= 2){
+            return true;
+        }
+        return false;
+    }
+};
