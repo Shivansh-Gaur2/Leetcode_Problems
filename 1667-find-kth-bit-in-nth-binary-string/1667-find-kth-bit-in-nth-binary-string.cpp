e93dd9fe8c56136511e67be1258e50918bdaf1e4
Shivansh Gaur <shivanshgaur.bt21ele@pec.edu.in>
2024-10-19 08:39:28 +0530

    Time: 0 ms (100%), Space: 7.2 MB (98.81%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-void helper(string& s) {
-    for (int i = 0; i < s.size(); i++) {
-        s[i] = (s[i] == '0') ? '1' : '0';  
-    }
-    // cout << s << endl;
-}
-
     char findKthBit(int n, int k) {
-        string s = "0";
-        while(n != 1){
-            string s1 = s;
-            helper(s1);
-            reverse(s1.begin(), s1.end());
-            s = s + "1" + s1;
-            n--;
+        if (n == 1) {
+            return '0';  
         }
-        cout << s << endl;
-        return s[k-1];
+        
+        int length = (1 << n) - 1;  
+        int mid = length / 2 + 1;   
+        
+        if (k == mid) {
+            return '1';
+        } else if (k < mid) {
+            return findKthBit(n - 1, k);  
+        } else {
+            return findKthBit(n - 1, length - k + 1) == '0' ? '1' : '0';
+        }
     }
 };
