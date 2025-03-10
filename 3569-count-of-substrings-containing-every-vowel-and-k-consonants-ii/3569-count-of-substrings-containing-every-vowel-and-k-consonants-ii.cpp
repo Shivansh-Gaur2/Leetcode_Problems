dc749f54b7c1e1855f327971f396c2d939d72760
Shivansh Gaur <shivanshgaur.bt21ele@pec.edu.in>
2025-03-10 15:35:11 +0530

    Time: 197 ms (52.33%), Space: 48.7 MB (56.99%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -1,66 +1,30 @@
 class Solution {
 public:
-    bool isVowel(char ch){
-        return (ch == 'a') || (ch == 'e') || (ch == 'i') || (ch == 'o') || (ch == 'u');
-    }
+    long long f(string s, int k) {
+        unordered_map<char, int> ump;
+        long long ans = 0, l = 0, c = 0;
+        for (int r = 0; r < s.size(); r++) {
+            isVowel(s[r]) ? ump[s[r]]++ : c++;
 
-    long long countOfSubstrings(string word, int k) {
-        unordered_map<char,int> mp;
-        long long ans = 0;
-
-        int n = word.size();
-        vector<int> pre(n , 0);
-
-        if(!isVowel(word[0])){
-            pre[0] = 1;
-        }
-
-        for(int i=1;i<n;i++){
-            if(!isVowel(word[i])){
-                pre[i] = pre[i-1] + 1;
-            }else{
-                pre[i] = pre[i-1];
-            }
-        }
-        int i = 0;
-        int j = 0;
-
-        int cntk = 0;
-
-        while(j < n){
-            if(isVowel(word[j])){
-                mp[word[j]]++;
-            }else{
-                cntk++;
-            }
-
-            while(cntk > k){
-                if(isVowel(word[i])){
-                    mp[word[i]]--;
-                    if(mp[word[i]] == 0){
-                        mp.erase(word[i]);
-                    }
-                }else{
-                    cntk--;
+            while (ump.size() == 5 && c >= k) {
+                ans += s.size() - r;
+                if(isVowel(s[l])) {
+                    if (--ump[s[l]] == 0)
+                        ump.erase(s[l]);
+                } else {
+                    c--;
                 }
-                i++;
+                l++;
             }
-
-            while(mp.size() >= 5 && cntk == k){
-                int ub = upper_bound(pre.begin() , pre.end() , pre[j]) - pre.begin();
-                ans += (long long)(ub - j);
-                if(isVowel(word[i])){
-                    mp[word[i]]--;
-                    if(mp[word[i]] == 0){
-                        mp.erase(word[i]);
-                    }
-                }else{
-                    cntk--;
-                }
-                i++;
-            }
-            j++;
         }
         return ans;
+    }
+    bool isVowel(char ch) {
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            return true;
+        return false;
+    }
+    long long countOfSubstrings(string word, int k) {
+        return f(word, k) - f(word, k + 1);
     }
 };
